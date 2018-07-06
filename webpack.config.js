124a1095bb622209fabbc6e521f0948b730c06b0
Madan Sapkota <code@madansapkota.com>
2018-07-06 11:02:32 +0545

    webpack config for Vanilla JS.

---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+let path = require('path');
+// html helper/copier
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// files copier
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+// uglify plugin
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = (env, argv) => ({
+    entry: {
+        'background': './src/js/background.js',
+        'content': './src/js/content.js',
+        'option': './src/js/option.js',
+        'popup': './src/js/popup.js'
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/',
+        filename: 'js/[name].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'background.html',
+            template: 'src/background.html',
+            inject: true,
+            chunks: ['background']
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'option.html',
+            template: 'src/option.html',
+            inject: true,
+            chunks: ['option']
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'popup.html',
+            template: 'src/popup.html',
+            inject: true,
+            chunks: ['popup']
+        }),
+        // copy static files
+        new CopyWebpackPlugin([
+            {from: './src/assets', to: 'assets'},
+            {from: './src/_locales', to: '_locales'},
+            {from: './src/manifest.json', to: 'manifest.json'}
+        ])
+    ],
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|otf|eot|ttf)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10240,
+                            name: 'fonts/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    emitFile: false,
+                    name: '[name].[ext]?v=[hash]'
+                }
+            }
+        ]
+    },
+    performance: {
+        hints: false
+    },
+    optimization: {
+        runtimeChunk: false,
+        minimize: (argv.mode === 'production'),
+        minimizer: (argv.mode === 'production' && (argv.uglify === undefined || argv.uglify === 'true')) ? [
+            new UglifyJsPlugin({
+                cache: false,
+                parallel: true,
+                sourceMap: true
+            })
+        ] : []
+    },
+    devtool: (argv.mode === 'production') ? '' : '#source-map'
+});
