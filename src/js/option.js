f14676dba61fffe408f4fac8126ff0c0855b75b3
Madan Sapkota <code@madansapkota.com>
2018-09-01 23:49:51 +0545

    fix variable name.

--- src/js/option.js
+++ src/js/option.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Popup from '../components/Option';
+import Option from '../components/Option';
 import '../styles/option.scss';
 // render the app
-ReactDOM.render(<Popup/>, document.getElementById('option-app'));
+ReactDOM.render(<Option/>, document.getElementById('option-app'));
