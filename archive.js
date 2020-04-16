2787e38b382a5e9bbe4c186ce33ea7cd661f516e
Madan Sapkota <code@madansapkota.com>
2020-04-16 12:56:41 +0545

    Create file to archive dist directory

---
+++ archive.js
@@ -0,0 +1,18 @@
+// init fs module
+const fs = require('fs');
+
+const archiver = require('archiver');
+
+const output = fs.createWriteStream(__dirname + '/dist.zip');
+const archive = archiver('zip', {
+  zlib: {
+    level: 9,
+  }
+});
+
+// pipe archive
+archive.pipe(output);
+// append files
+archive.directory('dist/', false);
+// finalize the archive
+archive.finalize();
