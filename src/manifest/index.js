12df181600bc67e45661ef86091dc9f204fd6f29
Madan Sapkota <code@madansapkota.com>
2021-06-11 11:13:04 +0545

    Upgrade configs to support manifest v3

--- src/manifest/index.js
+++ src/manifest/index.js
@@ -1,50 +1,44 @@
 class Manifest {
   constructor() {
     return {
-      'manifest_version': 2,
-      'name': process.env.npm_package_app_name,
-      'short_name': process.env.npm_package_app_name_short,
-      'description': process.env.npm_package_description,
-      'default_locale': 'en',
-      'version': process.env.npm_package_version,
-      'minimum_chrome_version': JSON.stringify(56),
-      'author': process.env.npm_package_author_name,
-      'icons': {
-        '16': 'assets/img/16.png',
-        '48': 'assets/img/48.png',
-        '128': 'assets/img/128.png',
+      manifest_version: 3,
+      name: process.env.npm_package_app_name,
+      short_name: process.env.npm_package_app_name_short,
+      description: process.env.npm_package_description,
+      default_locale: 'en',
+      version: process.env.npm_package_version,
+      minimum_chrome_version: JSON.stringify(88),
+      author: process.env.npm_package_author_name,
+      icons: {
+        16: 'assets/img/16.png',
+        48: 'assets/img/48.png',
+        128: 'assets/img/128.png',
       },
-      'browser_action': {
-        'default_icon': {
-          '19': 'assets/img/19.png',
-          '38': 'assets/img/38.png',
+      action: {
+        default_icon: {
+          16: 'assets/img/16.png',
+          24: 'assets/img/24.png',
+          32: 'assets/img/32.png',
         },
-        'default_popup': 'popup.html',
+        default_title: process.env.npm_package_app_name_short,
+        default_popup: 'popup.html',
       },
-      'options_page': 'option.html',
-      'background': {
-        'page': 'background.html',
-        'persistent': false,
+      options_page: 'option.html',
+      background: {
+        service_worker: 'js/worker.js',
       },
-      'content_scripts': [
+      content_scripts: [
         {
-          'matches': [
-            '<all_urls>',
-          ],
-          'js': [
-            `js/content.js`,
-          ],
-          'match_about_blank': false,
-          'all_frames': false,
+          matches: ['<all_urls>'],
+          js: [`js/content.js`],
+          match_about_blank: false,
+          all_frames: false,
         },
       ],
-      'permissions': [
-        'tabs',
-        '<all_urls>',
-      ],
-      'web_accessible_resources': [],
+      permissions: ['tabs'],
+      host_permissions: ['<all_urls>'],
+      web_accessible_resources: [],
     };
-
   }
 }
 
