c7b65ceac60cb3882ac9f3a5d38716f9922c7687
Madan Sapkota <code@madansapkota.com>
2021-07-30 20:11:16 +0545

    Test notification API

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello, Background!');
+chrome.notifications.create('Hello', {
+  title: 'Drink Water',
+  contextMessage: 'Keep you hydrated!',
+  message: "Happiness is not the absence of problems, it's the ability to deal with them.",
+  iconUrl: '/icons/128.png',
+  type: 'basic',
+  buttons: [{ title: 'Done' }, { title: 'Cancel' }],
+});
