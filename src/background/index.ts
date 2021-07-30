43a4a1449e694bfc7211222dacfa162b2d92579e
Madan Sapkota <code@madansapkota.com>
2021-07-30 20:43:00 +0545

    Format message

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,7 +1,7 @@
 chrome.notifications.create('Hello', {
   title: 'Drink Water',
-  contextMessage: 'Keep you hydrated!',
-  message: "Happiness is not the absence of problems, it's the ability to deal with them.",
+  contextMessage: '',
+  message: 'Things may come to those who wait, but only the things left by those who hustle.\n— Abraham Lincoln',
   iconUrl: '/icons/128.png',
   type: 'basic',
   buttons: [{ title: 'Done' }, { title: 'Cancel' }],
