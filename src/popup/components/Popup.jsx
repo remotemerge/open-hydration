f2efead4a733b94c5d8bf1faa166a29fcab0814a
Madan Sapkota <code@madansapkota.com>
2021-07-30 15:31:12 +0545

    Format code

--- src/popup/components/Popup.jsx
+++ src/popup/components/Popup.jsx
@@ -1,17 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 export default class Popup extends Component {
   constructor(props) {
     super(props);
     // init variables
     this.state = {
-      title: 'Hello, Popup!'
-    }
+      title: 'Hello, Popup!',
+    };
   }
 
   render() {
-    return (
-      <div>{this.state.title}</div>
-    );
+    return <div>{this.state.title}</div>;
   }
 }
