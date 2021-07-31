99799e301142daf38f0a466d428a5b4efec1ab5d
Madan Sapkota <code@madansapkota.com>
2021-07-31 17:44:18 +0545

    Drop 100vh fix plugin

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,6 @@ const isProduction = process.env.NODE_ENV === 'production';
 module.exports = {
   plugins: [
     require('tailwindcss')('tailwind.config.js'),
-    require('postcss-100vh-fix'),
     require('autoprefixer'),
     require('@fullhuman/postcss-purgecss')({
       content: ['./src/*/components/*.jsx', './public/*.html'],
