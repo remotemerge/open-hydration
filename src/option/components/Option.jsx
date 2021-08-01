0418b194ee00162bd438a19fb115f62813853eac
Madan Sapkota <code@madansapkota.com>
2021-08-01 14:03:35 +0545

    Set background gradient

--- src/option/components/Option.jsx
+++ src/option/components/Option.jsx
@@ -4,5 +4,9 @@ import React from 'react';
 import '../styles/option.scss';
 
 export default function Option() {
-  return <div className="option">Hello, World!</div>;
+  return (
+    <div className="w-screen h-screen bg-gradient-to-b from-blue-100 to-blue-500">
+      <div className="option">Hello, World!</div>
+    </div>
+  );
 }
