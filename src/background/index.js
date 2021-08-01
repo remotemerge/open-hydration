1dc05a208eae4faccd9a75e23e254dbc9bfb7cad
Madan Sapkota <code@madansapkota.com>
2021-08-01 10:05:46 +0545

    Design system notification format

--- src/background/index.js
+++ src/background/index.js
@@ -1,8 +1,8 @@
 chrome.notifications.create('Hello', {
   title: 'Drink Water',
-  contextMessage: '',
   message: 'Things may come to those who wait, but only the things left by those who hustle.\n— Abraham Lincoln',
   iconUrl: '/icons/128.png',
   type: 'basic',
-  buttons: [{ title: 'Done' }, { title: 'Cancel' }],
+  requireInteraction: true,
+  buttons: [{ title: 'Next → 12:35 PM' }],
 });
