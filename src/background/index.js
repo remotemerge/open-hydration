19579191c0c839910229f51c7f3a51103764a95f
Madan Sapkota <code@madansapkota.com>
2021-08-02 17:09:18 +0545

    Test reminder with timeout

--- src/background/index.js
+++ src/background/index.js
@@ -1,8 +1,14 @@
-chrome.notifications.create('Hello', {
-  title: 'Drink Water',
-  message: 'Things may come to those who wait, but only the things left by those who hustle.\n— Abraham Lincoln',
-  iconUrl: '/icons/128.png',
-  type: 'basic',
-  requireInteraction: true,
-  buttons: [{ title: 'Close' }, { title: 'Next → 12:35 PM' }],
-});
+const notify = () => {
+  chrome.notifications.create('Hello', {
+    title: 'Drink Water',
+    message: 'Things may come to those who wait, but only the things left by those who hustle.\n— Abraham Lincoln',
+    iconUrl: '/icons/128.png',
+    type: 'basic',
+    requireInteraction: false,
+    buttons: [{ title: 'Close' }, { title: 'Next → 12:35 PM' }],
+  });
+};
+
+setTimeout(() => {
+  notify();
+}, 10 * 1000);
