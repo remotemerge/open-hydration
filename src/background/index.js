71e8a770de9a41ef7de37aff663d5ad00380a11d
Madan Sapkota <code@madansapkota.com>
2021-08-03 09:22:53 +0545

    Change notification priority

--- src/background/index.js
+++ src/background/index.js
@@ -1,14 +1,9 @@
-const notify = () => {
-  chrome.notifications.create('Hello', {
-    title: 'Drink Water',
-    message: 'Things may come to those who wait, but only the things left by those who hustle.\n— Abraham Lincoln',
-    iconUrl: '/icons/128.png',
-    type: 'basic',
-    requireInteraction: false,
-    buttons: [{ title: 'Close' }, { title: 'Next → 12:35 PM' }],
-  });
-};
-
-setTimeout(() => {
-  notify();
-}, 10 * 1000);
+chrome.notifications.create('Hello', {
+  title: 'Drink Water',
+  message: 'Things may come to those who wait, but only the things left by those who hustle.\n— Abraham Lincoln',
+  iconUrl: '/icons/128.png',
+  type: 'basic',
+  requireInteraction: false,
+  buttons: [{ title: 'Close' }, { title: 'Next → 12:35 PM' }],
+  priority: 2,
+});
