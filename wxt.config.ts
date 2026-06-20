a5c33634ef2ef7765d4252c8832e3e36ed04d21b
Madan Sapkota <contact@madansapkota.com>
2026-06-20 21:57:54 +0545

    Add Tailwind CSS plugin

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'wxt';
+import tailwindcss from "@tailwindcss/vite";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  vite: () => ({
+    plugins: [tailwindcss()],
+  }),
   modules: ['@wxt-dev/module-react'],
   manifestVersion: 3,
   entrypointsDir: 'src',
