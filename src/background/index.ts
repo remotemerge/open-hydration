9f5fb7631dec760f6bd5ac3d5fd649b7d3d183b3
Madan Sapkota <contact@madansapkota.com>
2026-06-20 21:19:32 +0545

    Migrate script to use WXT and browser API

--- src/background/index.ts
+++
@@ -1,9 +0,0 @@
-chrome.notifications.create('Hello', {
-  title: 'Drink Water',
-  message: 'Things may come to those who wait, but only the things left by those who hustle.\n— Abraham Lincoln',
-  iconUrl: '/icons/128.png',
-  type: 'basic',
-  requireInteraction: false,
-  buttons: [{ title: 'Close' }, { title: 'Next → 12:35 PM' }],
-  priority: 2,
-});
