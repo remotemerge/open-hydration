a4f72967a1064517905646f32965e62603e875ce
Madan Sapkota <contact@madansapkota.com>
2026-06-21 21:45:02 +0545

    Remove SaveBar component

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -6,11 +6,14 @@ import Notifications from './components/Notifications';
 import Appearance from './components/Appearance';
 import DataPrivacy from './components/DataPrivacy';
 import OpenSource from './components/OpenSource';
-import SaveBar from './components/SaveBar';
+import Toast from './components/Toast';
 import { useTheme } from './hooks/useTheme';
+import { useSaveToast } from './hooks/useSaveToast';
 
 export default function App() {
   useTheme();
+  const { toast, dismiss } = useSaveToast();
+
   return (
     <div className="flex min-h-screen justify-center bg-bg px-6 pb-16 pt-11 font-sans text-fg antialiased">
       <div className="flex w-full max-w-180 flex-col gap-4.5">
@@ -40,8 +43,9 @@ export default function App() {
         <Appearance />
         <DataPrivacy />
         <OpenSource />
-        <SaveBar />
       </div>
+
+      {toast && <Toast message={toast} onDone={dismiss} />}
     </div>
   );
 }
