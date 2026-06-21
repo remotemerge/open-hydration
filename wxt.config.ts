ca4a6d74ea9f460fccccf5199cf82a56e0a1e4ba
Madan Sapkota <contact@madansapkota.com>
2026-06-21 09:41:44 +0545

    Fix quotation style

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'wxt';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
