5b281001153ff4f2ce233b665f4c5904035a80fd
Madan Sapkota <contact@madansapkota.com>
2026-06-22 14:36:01 +0545

    Add drinks table schema

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,14 +1,17 @@
 import { Dexie, type EntityTable } from 'dexie';
 import { defaultSettings, type Settings } from './settings';
+import type { DrinkDay } from './drinks';
 
-// IndexedDB store holding the single document
+// IndexedDB store holding settings and daily drink logs
 const db = new Dexie('OpenHydration') as Dexie & {
   settings: EntityTable<Settings, 'id'>;
+  drinks: EntityTable<DrinkDay, 'id'>;
 };
 
 // Set the database version and schema
 db.version(1).stores({
   settings: 'id',
+  drinks: 'id',
 });
 
 // Seeds default settings once, on initial database creation
