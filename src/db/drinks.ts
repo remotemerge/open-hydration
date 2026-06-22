acf1da3a8bcde7161bffc1ad9cc830582665433d
Madan Sapkota <contact@madansapkota.com>
2026-06-22 16:45:27 +0545

    Update the schema and add methods

--- src/db/drinks.ts
+++ src/db/drinks.ts
@@ -1,15 +1,18 @@
 export interface DrinkDay {
   id: string;
   glasses: number;
+  goal?: number; // Frozen per day
 }
 
-/**
- * Get the current date in YYYY-MM-DD format
- */
+// The local date in format YYYY-MM-DD.
 const dateFormatter = new Intl.DateTimeFormat('en-CA', {
   year: 'numeric',
   month: '2-digit',
   day: '2-digit',
 });
 
-export const getTodayId = (): string => dateFormatter.format(new Date());
+// Get the ID for a given date.
+export const dateIdFor = (date: Date): string => dateFormatter.format(date);
+
+// Get the ID for today's date.
+export const getTodayId = (): string => dateIdFor(new Date());
