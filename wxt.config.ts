31c5d8d191455a81961980a0a8dfc9e56a7bca6d
Madan Sapkota <contact@madansapkota.com>
2026-06-23 20:26:45 +0545

    Add permissions for alarms and notifications

--- wxt.config.ts
+++ wxt.config.ts
@@ -19,5 +19,6 @@ export default defineConfig({
     short_name: '__MSG_extShortName__',
     description: '__MSG_extDescription__',
     default_locale: 'en',
+    permissions: ['alarms', 'notifications'],
   },
 });
