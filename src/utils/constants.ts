1970bb8cb7221acb9b64f75ad4cc5b3fe7345d96
Madan Sapkota <contact@madansapkota.com>
2026-06-23 11:11:31 +0545

    Add hydration constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+// Millilitres of water per standard glass
+export const ML_PER_GLASS = 250;
+
+// Minimum daily hydration goal in glasses
+export const MIN_GOAL = 1;
+
+// Average daily hydration goal in glasses
+export const AVG_GOAL = 8;
+
+// Maximum daily hydration goal in glasses
+export const MAX_GOAL = 16;
